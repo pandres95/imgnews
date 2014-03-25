03e4e689da6eb63472a71c7eca474a162145e40e
Pablo Dorado <pablo@amagi4all.com>
2014-03-25 00:49:12 -0500

    Added templates and static files support

--- pinteArg/baseMod/views.py
+++ pinteArg/baseMod/views.py
@@ -1,5 +1,5 @@
-from django.shortcuts import render
+from django.shortcuts import render, render_to_response
 
 # Create your views here.
 def inicio(request):
-    return render_to_response('base.html', { 'authenticated': False } )
+    return render_to_response('init.html', { 'authenticated': True, 'title': 'Inicio' } )
--- pinteArg/pinteArg/urls.py
+++ pinteArg/pinteArg/urls.py
@@ -3,10 +3,10 @@ from django.conf.urls import patterns, include, url
 from django.contrib import admin
 admin.autodiscover()
 
-urlpatterns = patterns('',
+urlpatterns = patterns('baseMod',
     # Examples:
     # url(r'^$', 'pinteArg.views.home', name='home'),
     # url(r'^blog/', include('blog.urls')),
-
+    url(r'', 'views.inicio'),
     url(r'^admin/', include(admin.site.urls)),
 )
