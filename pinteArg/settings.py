f4b673266fafdc75f73b6e1fcea871c2e9f668d0
Pablo Dorado <pablo@amagi4all.com>
2014-04-06 00:38:44 -0500

    Restored static location

--- pinteArg/settings.py
+++ pinteArg/settings.py
@@ -87,6 +87,6 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.6/howto/static-files/
 
-STATIC_URL = BASE_DIR + '/static/'
+STATIC_URL = '/static/'
 MEDIA_ROOT = BASE_DIR + '/media/'
 MEDIA_URL = '/media/'
